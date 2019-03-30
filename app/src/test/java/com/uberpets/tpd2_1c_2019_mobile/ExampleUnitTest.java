7cd9df224d4c49d5f2e2ed6130dcdaffb13e741f
ruben <rbnm.jimenez@gmail.com>
2019-03-30 17:14:56 -0300

    init android

---
+++ app/src/test/java/com/uberpets/tpd2_1c_2019_mobile/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.uberpets.tpd2_1c_2019_mobile;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
