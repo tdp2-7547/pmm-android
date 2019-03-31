66cd34221ad270485e0fba1b79303f0274e7c599
ruben <rbnm.jimenez@gmail.com>
2019-03-30 23:00:31 -0300

    show your location with zoom in maps

--- app/src/main/java/com/uberpets/tpd2_1c_2019_mobile/MapsActivity.java
+++ app/src/main/java/com/uberpets/tpd2_1c_2019_mobile/MapsActivity.java
@@ -1,29 +1,67 @@
 package com.uberpets.tpd2_1c_2019_mobile;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Location currentLocation;
+    private FusedLocationProviderClient mfusedLocationProviderClient;
+    private static float ZOOM_VALUE = 14.0f;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+
+        //is used to obtain user's location, with this our app no needs to manually manage connections
+        //to Google Play Services through GoogleApiClient
+        mfusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        fetchLastLocation();
     }
 
+
+    public void fetchLastLocation() {
+
+         //check if user has granted location permission,
+         // its necessary to use mfusedLocationProviderClient
+        if (ActivityCompat.checkSelfPermission(MapsActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MapsActivity.this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }else{
+            // obtain the last location and save in task, that's Collection's Activities
+            Task<Location> task = mfusedLocationProviderClient.getLastLocation();
+            // add object OnSuccessListener, when the connection is established and the location is fetched
+            task.addOnSuccessListener(new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null) {
+                        currentLocation = location;
+                        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+                        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                                .findFragmentById(R.id.map);
+                        mapFragment.getMapAsync(MapsActivity.this);
+                    }
+                }
+            });
+        }
+    }
 
     /**
      * Manipulates the map once available.
@@ -38,9 +76,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng latLng = new LatLng(currentLocation.getLatitude(),currentLocation.getLongitude());
+
+        //MarkerOptions are used to create a new Marker.You can specify location, title etc with MarkerOptions
+        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("Estas Acá");
+
+        //Adding the created the marker on the map
+        mMap.addMarker(markerOptions);
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,ZOOM_VALUE));
     }
+
 }
