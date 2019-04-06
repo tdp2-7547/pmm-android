416f470d04fc422b5323ab6d661d7fc3ad0b5e24
federico longhi <federico.longhi@cloudgaia.com>
2019-04-06 16:15:46 -0300

    algunos tests de botones

--- app/src/main/java/com/uberpets/tpd2_1c_2019_mobile/DriverHome.java
+++ app/src/main/java/com/uberpets/tpd2_1c_2019_mobile/DriverHome.java
@@ -1,8 +1,11 @@
 package com.uberpets.tpd2_1c_2019_mobile;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,8 +16,24 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
 public class DriverHome extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private Location currentLocation;
+    private FusedLocationProviderClient mfusedLocationProviderClient;
+    private static float ZOOM_VALUE = 14.0f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +50,10 @@ public class DriverHome extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        mfusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        fetchLastLocation();
     }
 
     @Override
@@ -88,5 +111,45 @@ public class DriverHome extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng latLng = new LatLng(currentLocation.getLatitude(),currentLocation.getLongitude());
+
+        //MarkerOptions are used to create a new Marker.You can specify location, title etc with MarkerOptions
+        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("Estas Acá");
+
+        //Adding the created the marker on the map
+        mMap.addMarker(markerOptions);
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,ZOOM_VALUE));
+    }
+
+    public void fetchLastLocation() {
+
+        //check if user has granted location permission,
+        // its necessary to use mfusedLocationProviderClient
+        if (ActivityCompat.checkSelfPermission(DriverHome.this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(DriverHome.this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }else{
+            // obtain the last location and save in task, that's Collection's Activities
+            Task<Location> task = mfusedLocationProviderClient.getLastLocation();
+            // add object OnSuccessListener, when the connection is established and the location is fetched
+            task.addOnSuccessListener(new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null) {
+                        currentLocation = location;
+                        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+                        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                                .findFragmentById(R.id.map);
+                        mapFragment.getMapAsync(DriverHome.this);
+                    }
+                }
+            });
+        }
     }
 }
